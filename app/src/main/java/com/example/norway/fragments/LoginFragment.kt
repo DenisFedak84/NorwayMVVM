83d00fba1adacb12da0646628b31a217c9fad8e5
Denys Fedak <denisfedak84@gmail.com>
2023-05-19 19:05:17 +0200

    Added Login flow

--- app/src/main/java/com/example/norway/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/norway/fragments/LoginFragment.kt
@@ -1,22 +1,27 @@
 package com.example.norway.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.norway.activity.LoginActivity
 import com.example.norway.databinding.FragmentLoginBinding
 import com.example.norway.lifecycle.observeEvents
 import com.example.norway.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 private const val ARG_USER_NAME = "arg_user_name"
 
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
     private var userName: String? = null
     private lateinit var binding: FragmentLoginBinding
-    private val viewModel: LoginViewModel by viewModels()
+    val viewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,19 +62,37 @@ class LoginFragment : Fragment() {
         binding.tvNewUser.setOnClickListener {
             (activity as LoginActivity).navigateToCreateUserFragment()
         }
+
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPwd.text.toString()
+            println("My user $email $password")
+            viewModel.login(email, password)
+        }
+
+        binding.etEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
     }
 
     private fun showToken(token: String) {
-
+        Toast.makeText(context, "Token: ${token}", Toast.LENGTH_LONG).show()
     }
 
 
     companion object {
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(name: String) =
             LoginFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_USER_NAME, param1)
+                    putString(ARG_USER_NAME, name)
                 }
             }
     }
--- app/src/main/java/com/example/norway/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/norway/viewmodel/LoginViewModel.kt
@@ -2,16 +2,39 @@ package com.example.norway.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.norway.data.LoginRequest
+import com.example.norway.data.LoginResponse
 import com.example.norway.lifecycle.ConsumableEvent
+import com.example.norway.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(): ViewModel() {
+class LoginViewModel @Inject constructor(private val repository: UserRepository) : ViewModel() {
+
     val eventsLiveData = MutableLiveData<ConsumableEvent<Event>>()
 
 
+    fun login(email: String, password: String) {
 
+        viewModelScope.launch {
+            repository.loginByObject(LoginRequest(email, password)).flowOn(Dispatchers.IO)
+                .catch { error -> }
+                .map {
+                    it.accessToken
+                }
+                .collect {
+                    eventsLiveData.value = ConsumableEvent(Event.ShowToken(it))
+                }
+        }
+    }
 
     sealed class Event {
         class ShowToken(val token: String) : Event()
